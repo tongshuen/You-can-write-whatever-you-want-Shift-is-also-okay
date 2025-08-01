66e5854115177aeb9174dfc6c5d9e7eb0e2f5ec9
童顺 <15730642468@163.com>
2025-08-01 15:34:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
- print("Hello world")
+HXD3C="""=========================================
+                   /———\
+                    //
+                    \\
+ /-----------------------------------\
+|              / 和谐      \           |
+|            /        HXD3C \         |
+ \------------------------------------/
+    \_·_/                       \_·_/
+An HXD3C happily running on the railway
+"""
+print("{HXD3C}")
+#Don't ask me why I don't directly print(HXD3c), this is the essence of this project: do whatever you want, do whatever you want
