5fa6f9f64a5f70fe1d528b703b980bb388a9c45c
童顺 <15730642468@163.com>
2025-08-01 15:37:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-HXD3C="""=========================================
-                   /———\
+HXD3C="""========================================= Contact wire
+                   /———\ pantograph
                     //
                     \\
- /-----------------------------------\
-|              / 和谐      \           |
+ /-----------------------------------\ Car body
+|              / 和谐      \           | 
 |            /        HXD3C \         |
  \------------------------------------/
-    \_·_/                       \_·_/
+    \_·_/                       \_·_/ wheel
 An HXD3C happily running on the railway
 """
 print(f"{HXD3C}")
-#Don't ask me why I don't directly print(HXD3c), this is the essence of this project: do whatever you want, do whatever you want
+#Don't ask me why I don't directly print(HXD3C), this is the essence of this project: do whatever you want, do whatever you want
